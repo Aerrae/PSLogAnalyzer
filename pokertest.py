0f1c08eb4931eaa16f335ccee7bb3dfce8ff375e
Aerrae <rintamakij@gmail.com>
2017-06-18 14:12:24 +0300

    Parser update
    Updated parser to use regexp instead of direct string mathcing and added basic card statistic proto. New version is safer against player names with keywords or whitespace in them.

--- pokertest.py
+++ pokertest.py
@@ -15,23 +15,31 @@ playerdb = []
 class Player():
     def __init__(self,player_name,hand_nr):
         self.name = player_name
-        self.associated_hands = [hand_nr]
+        self.associated_hands = {hand_nr : [0.0,""]}
         self.gains = 0.0
-        
+        self.current_hand = hand_nr
         #takes into account only the first preflop action
         self.preflop_actions = 0
         self.reached_flop = 0
         self.reached_turn = 0
         self.reached_river = 0
-    def add_hand_nr(self, hand_nr):        
-        self.associated_hands.append(hand_nr)
+    def add_hand_nr(self, hand_nr):
+        self.current_hand = hand_nr
+        #print(hand_nr)
+        #self.associated_hands.append(hand_nr)
+        self.associated_hands[self.current_hand] = [0.0,""]
+        
+    def set_cards(self,cards):
+        self.associated_hands[self.current_hand][1] = cards
+    
         
     def gain(self,amount):
         self.gains += amount
+        self.associated_hands[self.current_hand][0] += amount
         
     def bet(self,amount):
         self.gains -= amount
-        
+        self.associated_hands[self.current_hand][0] -= amount  
     def calc_avg_gain(self):
         #associated hands can't be zero and players without association to any hand shouldn't be added
         return self.gains/len(self.associated_hands)
@@ -65,6 +73,12 @@ class Player():
             self.reached_river += 1
         
     
+class Card_proto:
+    def __init__(self, cards, gains):
+        self.cards = cards
+        self.gains = gains
+    def gain(self, gains):
+        self.gains += gains
     
 
 
@@ -117,23 +131,58 @@ def handle_logfile(logfile):
     def getKey(player):
         return player.name
     for player in sorted(playerdb,key=getKey):
-        print("player "+player.name + " has gained: {0:.2f}, preflop_activity {1:.2f}, flop {2:.2f} turn: {3:.2f}, river {4:.2f},".format( player.gains, player.calc_activity(), \
-        player.calc_flop_activity(),player.calc_turn_activity(),player.calc_river_activity()))   
+        if player.name == "Velinektori":
+            print(logfile.split("/")[-1])
+            print("player "+player.name + " has gained: {0:.2f}, preflop_activity {1:.2f}, flop {2:.2f} turn: {3:.2f}, river {4:.2f},".format( player.gains, player.calc_activity(), \
+            player.calc_flop_activity(),player.calc_turn_activity(),player.calc_river_activity()))
+            pairs = 0
+            suitecards = 0
+            pair_amount = 0
+            suite_amount = 0
+            for key in player.associated_hands.keys():
+                card_data = player.associated_hands[key]
+                cards = card_data[1]
+                for land in ["s","h","d","c"]:
+                    if cards.count(land) > 1:
+                        suite_amount += 1
+                        
+                        suitecards += card_data[0]
+
+                valueregex = "([2-9ATKJQ9]).*([2-9ATKJQ])"
+                valuematch = re.match(valueregex,cards)
+                if valuematch:
+                    #print(valuematch.groups())
+                    if valuematch.group(1) == valuematch.group(2):
+                        #print(valuematch.groups())
+                        pair_amount += 1
+                        pairs += card_data[0]
+                #print("{0:.2f} {1}".format(card_data[0],card_data[1]))
+            if pair_amount:
+                print("pairs {0:.2f}".format(pairs/pair_amount))
+                print("suite cards {0:.2f}".format(suitecards/suite_amount))
+    #playerdb = []
         
 def handle_initial_state(data, gamestate):
+    #print(data)
     global playerdb
     list_data = data.split('\n')
     temp_hand = re.search("PokerStars Hand #(\d*)",list_data[0])
     
     hand_nr = temp_hand.group(1)
+    card_regex = "Dealt to (.*) \[([2-9ATJKQ][dhcs]) ([2-9ATJKQ][dhcs])\]"
     for line in list_data:
         if line.startswith('Seat'):
-            player = line.split()[2].strip()
+            player = re.match("(Seat [0-9]:) (.*) ([()])",line).group(2).strip()
+            #player = line.split()[2].strip()
             players = [x.name for x in playerdb]
             if player in players:
                 playerdb[players.index(player)].add_hand_nr(hand_nr)
             else:
                 playerdb.append(Player(player,hand_nr))
+        card_match = re.match(card_regex,line)
+        if card_match:
+            player, card1, card2 = card_match.groups()
+            playerdb[players.index(player)].set_cards("{0} {1}".format(card1, card2))
     handle_round(list_data[1:], gamestate)
 
 def handle_postflop(data, gamestate):
@@ -185,48 +234,41 @@ def handle_summary(data, gamestate):
 
 def bet_handler(line):
     global playerdb
-    line = line.split()
-    sign = -1
-    #print(line)
     if not line:
         #print("none")
         return None
-    
-    if "collected" in line[1]:
-        player_idx = 0
-        reward_idx = 2
-        sign = 1
-        action = "collected"
         
-    elif "Uncalled" in line[0] and "bet" in line[1]:
-        player_idx = -1
-        reward_idx = 2
-        sign = 1
-        line[2] = line[2][1:-1]
-        action = "Uncalled bet"
-    elif "posts" in line[1]:
-        player_idx = 0
-        reward_idx = -1
-        action = "posts"
-    elif line[1].strip() in ["bets","calls"]:
-        player_idx = 0
-        reward_idx = 2
-        action = line[1].strip()
-    elif line[1].strip() in ['raises']:
-        player_idx = 0
-        reward_idx = 4
-        action = line[1].strip()
-    else:
-        #print("none")
+    actregex = "(.*): (posts|bets|calls).*([€$£])([.0-9]*)"
+    actmatch = re.match(actregex,line)
+    player = None
+    if actmatch:
+        player,action,currency,amt_gained = actmatch.groups()
+        amt_gained = -1*float(amt_gained)
+
+    raiseregex = "(.*): (raises) ([€$£])([.0-9]*) to* [€$£]([.0-9]*)"
+    raisematch = re.match(raiseregex,line)
+    if raisematch:
+        player,action,currency,bet_increase,amt_gained = raisematch.groups()
+        amt_gained = -1*float(amt_gained)
+
+    uncregex = "(Uncalled bet)[ (]*([€$£])([.0-9]*).*returned to (.*)"
+    uncmatch = re.match(uncregex,line)
+    if uncmatch:
+        #print(uncmatch.groups())
+        action,currency,amt_gained,player = uncmatch.groups()
+        
+    collregex = "(.+) (collected) ([€$£])([.0-9]*) from (side|main)*.*pot"
+    collmatch = re.match(collregex,line)
+    if collmatch:
+        player,action,currency,amt_gained,side_or_main = collmatch.groups()
+
+    if not player:
         return None
-        
-    player = line[player_idx].strip().replace(":","")
-    amt_gained = sign*float(line[reward_idx][1:])
-    
-    return (player,amt_gained,action)
+    #print(line)
+    #print(amt_gained)
+    return (player,float(amt_gained),action)
         
 def update_bet_status(player,amt_gained):
-    
     playerdb[[x.name for x in playerdb].index(player)].gain(amt_gained)
     #print("player "+player+" gained: % .2f" %( amt_gained))
 
@@ -234,7 +276,7 @@ def main(argv):
     if len(argv) > 0:
         logfolder = argv[0]
     else:
-        logfolder = None #r'C:/Users/Aerrae/AppData/Local/PokerStars.EU/HandHistory/Velinektori/'
+        logfolder = r'C:/Programming/spyder/poker/pokerhands/'
         
     if not logfolder:
         print("No logfolder given, please edit the file or add as argument")
